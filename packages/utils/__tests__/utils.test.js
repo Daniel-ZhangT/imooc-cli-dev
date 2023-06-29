090704552eb73b8dc394ebe6692aafb419c9a068
zhangteng <danieltg672@gmail.com>
2023-06-29 10:23:32 +0800

    __init__

--- packages/utils/__tests__/utils.test.js
+++ packages/utils/__tests__/utils.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const utils = require('..');
-const assert = require('assert').strict;
+module.exports = utils
 
-assert.strictEqual(utils(), 'Hello from utils');
-console.info("utils tests passed");
+function utils() {
+
+}
